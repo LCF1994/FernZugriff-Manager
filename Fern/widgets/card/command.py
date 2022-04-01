5726cb142b7470b01d90b3ee103a246cba5cac7a
Ferreira <lucasferreira@siemens.com>
2022-04-01 15:12:51 -0300

    Commands now are following connection state

--- Fern/widgets/card/command.py
+++ Fern/widgets/card/command.py
@@ -1,6 +1,11 @@
 from auxiliary.common import CommonFeatures
 from auxiliary.servidor_paramiko import ServidorSAGE
-from kivy.properties import DictProperty, ObjectProperty, StringProperty
+from kivy.properties import (
+    BooleanProperty,
+    DictProperty,
+    ObjectProperty,
+    StringProperty,
+)
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
 from kivymd.uix.spinner import MDSpinner
@@ -10,18 +15,30 @@ from widgets.card.process import ProcessCard
 
 
 class CommandCard(MDCard, CommonFeatures):
+    name = StringProperty('')
+
     image = StringProperty('')
     title = StringProperty('Action')
     btn_icon = StringProperty('centos')
     btn_text = StringProperty('Executar')
+
+    btn_disable = BooleanProperty(True)
+
     target = ObjectProperty(ServidorSAGE)
+
     release_function = DictProperty()
 
     def on_kv_post(self, base_widget):
         self.app = MDApp.get_running_app()
-        self.screen = self.parent.parent.parent
+        self.app.widgets[self.target.name][
+            f'COMMAND_CARD_{self.name.upper()}'
+        ] = self
 
+        self.screen = self.parent.parent.parent
         return super().on_kv_post(base_widget)
+
+    def update_connection(self, conn_state: bool) -> None:
+        self.btn_disable = not conn_state
 
     # Actions
     def open_visor_acesso(self, args: dict) -> None:
@@ -37,6 +54,5 @@ class CommandCard(MDCard, CommonFeatures):
         self.app._on_open_process_card(self.target)
 
     def execute_remote_command(self, args: dict):
-        self._get_screen()
         print(f'Executar comando no Servidor {self.target.host}')
         print(args)
